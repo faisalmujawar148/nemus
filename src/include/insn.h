88e005bb2b9ad91773cb5501fd41a1abd8ec9b76
faisalmujawar148 <faisal.mujawar4@gmail.com>
2025-01-18 23:25:09 +0000

    Fix error in bit assignment

--- src/include/insn.h
+++ src/include/insn.h
@@ -41,10 +41,10 @@ static inline bool and_insn(Opcode opcode, cpu::NESCpu &cpu) {
     regblck.m_accm = accm & static_cast<uint8_t>(opr);
   };
   if(!(regblck.m_accm)){
-  	regblck.m_status = m_status_z;
+  	regblck.m_status = 00000010;
   };
   if(regblck.m_accm & 1000000){
-	regblck.m_status = m_status_n;
+	regblck.m_status = regblck.m_status & 10000000;
   };
 
 };
