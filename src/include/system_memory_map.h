69d2ee40708156e71905ea0ce0d2ac24582f19a4
faisalmujawar148 <faisal.mujawar4@gmail.com>
2025-01-18 01:27:33 +0000

    #1 semi-resolved added mappings

--- src/include/system_memory_map.h
+++ src/include/system_memory_map.h
@@ -1,8 +1,42 @@
-#include "typedefs.h"
+#include "typedefs.h" 
 #include "util.h"
 
 namespace nemus::sysmmap {
+// RAM ranges: 
+/** Zero Page Range. */
 constexpr range<Addr16> zero_page_range = range<Addr16>(
     0x0, 0x100, "Zero Page Address Range in the System Memory Map");
 
 } // namespace nemus::sysmmap
+
+/** Stack range. */
+constexpr range<Addr16> stack_range = range<Addr16>(0x0101, 0x0200, "Stack Address Range in the System Memory Map");
+
+/** Ram range. */
+constexpr range<Addr16> ram_range = range<Addr16>(0x0201, 0x0800, "RAM Address Range in the System Memory Map");
+/** Lower Mirror range. */
+constexpr range<Addr16> lower_mirror_range = range<Addr16>(0x0801, 0x2000, "Lower Mirror Address Range in the System Memory Map");
+
+// I/O ranges:
+/** Lower I/O Registers. */
+constexpr range<Addr16> lower_io_range = range<Addr16>(0x2001, 0x2008, "Lower I/O Address Range in the System Memory Map");
+/** Upper Mirror range. */
+constexpr range<Addr16> upper_mirror_range = range<Addr16>(0x2009, 0x4000, "Upper Mirror Address Range in the System Memory Map");
+/** Upper I/O Registers. */
+constexpr range<Addr16> upper_io_range = range<Addr16>(0x4001, 0x4020, "Upper I/O Address Range in the System Memory Map");
+
+// Expansion ROM
+/** Expansion ROM. */
+constexpr range<Addr16> expansion_rom_range = range<Addr16>(0x4021, 0x6000, "Expansion ROM Address Range in the System Memory Map");
+
+// SRAM 
+/** SRAM. */
+constexpr range<Addr16> sram_range = range<Addr16>(0x6001, 0x8000, "SRAM Address Range in the System Memory Map"); 
+
+
+// PRG-ROM 
+/** PRG-ROM Lower Bank. */
+constexpr range<Addr16> lower_prg_rom_range = range<Addr16>(0x8001, 0xC000, "PRG-ROM Lower Bank Address Range in the System Memory Map");
+/** PRG-ROM Upper Bank. */
+constexpr range<Addr16> upper_prg_rom_range = range<Addr16>(0xC001, 0x10000, "PRG-ROM Upper Bank Address Range in the System Memory Map");
+
